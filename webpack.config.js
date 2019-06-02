e6fab3373fcd0935b223c174b38fb5c409d294b1
Harmelodic <matt@harmelodic.com>
2019-06-02 10:55:23 +0100

    More linting and externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const SRC = __dirname + "/demo/";
-const PUBLIC = __dirname + "/public/";
+const SRC = __dirname + '/demo/';
+const PUBLIC = __dirname + '/public/';
 
 module.exports = {
-    entry: SRC + "index.js",
-    output: {
-        path: PUBLIC,
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                query: {
-                    presets: [
-                        "@babel/preset-react"
-                    ]
-                }
-            }
-        ]
-    }
+  entry: SRC + 'index.js',
+  output: {
+    path: PUBLIC,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [
+            '@babel/preset-react',
+          ],
+        },
+      },
+    ],
+  },
+  externals: {
+    'react': 'commonjs react',
+  },
 };
